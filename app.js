8c0abb0cef44d194c83f639eaaf7c029b0562bea
Santiago Alfonso <salfonsop@udistrital.edu.co>
2025-03-02 14:26:49 -0500

    Proyecto finalizado

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
 let amigos = [];
 
 function agregarAmigos(){
-    let nombre = document.getElementById('amigo');
-    if(nombre.value==''){
-        alert('Por favor ingrese un nombre valido');
+    let nombre = document.getElementById('amigo').value;
+    if(nombre==''){
+        alert('Por favor, ingrese un nombre valido');
+    }else{
+        amigos.push(nombre);
+    }
+    limpiar();
+    actualizarListaAmigos();
+}
+
+function limpiar(){
+    document.querySelector('#amigo').value='';
+}
+
+function actualizarListaAmigos(){
+    let lista = document.querySelector('#listaAmigos');
+    lista.innerHTML='';
+    let contenidoLista='';
+    for (let index = 0; index < amigos.length; index++) {
+        contenidoLista += `<li>${amigos[index]}</li>`;
+    }
+    lista.innerHTML=contenidoLista;
+}
+
+function sortearAmigo() {
+    if (!amigos.length) { 
+        console.log("El arreglo no tiene elementos");
+    } else {
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        let amigoAleatorio = amigos[indiceAleatorio];
+        document.querySelector('#resultado').innerHTML=`El amigo secreto sorteado es: ${amigoAleatorio}`;
     }
 }
