0c5933e20c49045bd4ecbccdc346f2f5696166d1
Santiago Alfonso <salfonsop@udistrital.edu.co>
2025-03-02 13:17:33 -0500

    tercer commit

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+let amigos = [];
+
+function agregarAmigos(){
+    let nombre = document.getElementById('amigo');
+    if(nombre.value==''){
+        alert('Por favor ingrese un nombre valido');
+    }
+}
